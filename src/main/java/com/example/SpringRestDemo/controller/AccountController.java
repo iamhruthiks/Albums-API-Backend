5b072bea2cafb7cb18fbc10e20e2d95291b8cfd2
iamhruthiks <iamhruthiks@gmail.com>
2025-02-23 09:49:36 +0530

    Section 44: Spring Boot Restful (Part 02) - Setting up - 379. S01L03 - Spring Boot, Starting with RestFul Api's

---
+++ src/main/java/com/example/SpringRestDemo/controller/AccountController.java
@@ -0,0 +1,12 @@
+package com.example.SpringRestDemo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AccountController {
+    @GetMapping("/")
+    public String demo() {
+        return "Hello World!";
+    }
+}
